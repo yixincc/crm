493932d5f5b15c50898d7ae0f17b8decec5d2245
yixincc <2213707068@qq.com>
2022-01-13 11:57:25 +0800

    提交main和pom

--- src/main/java/com/yjxxt/crm/mapper/SaleChanceMapper.java
+++
@@ -1,8 +0,0 @@
-package com.yjxxt.crm.mapper;
-
-import com.yjxxt.crm.base.BaseMapper;
-import com.yjxxt.crm.pojo.SaleChance;
-
-public interface SaleChanceMapper extends BaseMapper<SaleChance,Integer> {
-
-}
